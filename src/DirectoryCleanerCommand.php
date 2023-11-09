f3f123345c2b240dcef2bb75c35dc4ad02a7889c
softjkeee <softjkeee@gmail.com>
2023-11-09 17:24:23 +0300

    lv -> SalesRender

--- src/DirectoryCleanerCommand.php
+++ src/DirectoryCleanerCommand.php
@@ -5,7 +5,7 @@
  * @author Timur Kasumov aka XAKEPEHOK
  */
 
-namespace Leadvertex\Plugin\Components\DirectoryCleaner;
+namespace SalesRender\Plugin\Components\DirectoryCleaner;
 
 
 use RecursiveDirectoryIterator;
